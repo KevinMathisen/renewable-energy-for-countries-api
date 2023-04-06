d0422d4b850aa2b02d91f738e5e83bdebd2f788c
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-06 16:06:22 +0200

    feat: create newWebHook struct

--- utils/structs/structs.go
+++ utils/structs/structs.go
@@ -9,3 +9,12 @@ type CountryOutput struct {
 	Year       string `json:"year,omitempty"` //  suppress field if not defined, such as when returning mean percentage value.
 	Percentage string `json:"percentage"`
 }
+
+/*
+* Struct for decoding JSON POST request for Registration of Webhooks in Notification endpoint.
+ */
+type NewWebhook struct {
+	Url     string `json:"url"`
+	Country string `json:"country"`
+	Calls   string `json:"calls"`
+}
