c08724520acad858896fefefa9b3b2ac9db60e76
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-06 16:05:46 +0200

    feat: create countryOutput struct

--- utils/structs/structs.go
+++ utils/structs/structs.go
@@ -1,1 +1,11 @@
 package structs
+
+/*
+* Struct for encoding json response for RENEWABLES_CURRENT and RENEWABLES_HISTORY endpoints.
+ */
+type CountryOutput struct {
+	Name       string `json:"name"`
+	IsoCode    string `json:"isoCode"`
+	Year       string `json:"year,omitempty"` //  suppress field if not defined, such as when returning mean percentage value.
+	Percentage string `json:"percentage"`
+}
