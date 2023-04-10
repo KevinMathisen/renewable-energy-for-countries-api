ea992d20b6232eefb731849323294f1ff8b5c77e
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-10 13:27:45 +0200

    feat: create httpRequestFromUrl(...) function

--- handlers/handlerFunctions.go
+++ handlers/handlerFunctions.go
@@ -26,3 +26,38 @@ func respondToGetRequestWithJSON(w http.ResponseWriter, jsonBody interface{}) {
 	// Manually set response http status code to ok
 	w.WriteHeader(http.StatusOK)
 }
+
+/*
+Create a request and returns response from a specified URL using specified method
+
+	url		- URL to send request to
+	method	- Method of request
+
+	return	- http response from request or error
+*/
+func httpRequestFromUrl(url string, method string) (http.Response, error) {
+	// Create empty response in case of error
+	var response http.Response
+
+	// Create request
+	request, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return response, err
+	}
+
+	// Set content type to empty
+	request.Header.Add("content-type", "")
+
+	// Set up client
+	client := &http.Client{}
+	defer client.CloseIdleConnections()
+
+	// Issue http request
+	res, err := client.Do(request)
+	if err != nil {
+		return response, err
+	}
+
+	// Return response
+	return *res, nil
+}
