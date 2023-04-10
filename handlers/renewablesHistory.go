ca6c9080bf6d6b68cd23d7df27b3c8858b1dd9dd
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-10 23:42:37 +0200

    docs: pseudo code for future functions

--- handlers/renewablesHistory.go
+++ handlers/renewablesHistory.go
@@ -7,3 +7,31 @@ import (
 func RenewablesHistory(w http.ResponseWriter, r *http.Request) {
 
 }
+
+/*
+getHistoryPercentageOfRenewablesForCountries(countries, startYear, endYear, wantMean)	//History
+
+	if startYear or endYear not specified
+		set them as default values
+
+
+	if countires specified and not/no mean:
+		getRenewablesForCountriesByYears(isoList, startYear, endYear)					Global1
+
+	if countires specified and mean:
+		getMeanRenewablesFromCountries(isoList, startYear, endYear)						History1
+			for countires
+				getMeanRenewablesFromCountry(isoCode, startYear, endYEar)				History1.1
+
+
+	else if no countires specified and want mean
+		getMeanRenewablesFromAllCountries()												History2
+			for all countires
+				getMeanRenewablesFromCountry(startYear, endYEar)						History2.2
+
+	else if no countries specified and no mean:
+		getRenewablesForAllCountiresByYear(startYear, endYear)							Global2
+
+
+
+*/
