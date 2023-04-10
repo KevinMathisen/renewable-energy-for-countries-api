2a17d720a8e3c7b303195cd730177b3d5195bdba
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-10 13:08:05 +0200

    feat: create respondToGetRequestWithJSON(...) function

---
+++ handlers/handlerFunctions.go
@@ -0,0 +1,28 @@
+﻿package handlers
+
+import (
+	"assignment2/utils/constants"
+	"encoding/json"
+	"net/http"
+)
+
+/*
+Responds to GET request with JSON content and body specified
+
+	w			- Responsewriter
+	jsonBody	- Any struct which will be encoded into json and sent as response body
+*/
+func respondToGetRequestWithJSON(w http.ResponseWriter, jsonBody interface{}) {
+	// Write to content type field in response header
+	w.Header().Add("content-type", constants.CONT_TYPE_JSON)
+
+	// Encode content and write to response
+	err := json.NewEncoder(w).Encode(jsonBody)
+	if err != nil {
+		http.Error(w, "Error during encoding: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Manually set response http status code to ok
+	w.WriteHeader(http.StatusOK)
+}
