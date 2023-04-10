0419b3b75f21a420d31d92988e43002185f50937
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-10 18:45:05 +0200

    feat: create InitializeFirestore(...) function

--- utils/db/db_connection.go
+++ utils/db/db_connection.go
@@ -2,8 +2,11 @@
 
 import (
 	"context"
+	"log"
 
 	"cloud.google.com/go/firestore" // Firestore-specific support
+	firebase "firebase.google.com/go"
+	"google.golang.org/api/option"
 )
 
 // Firebase context used by Firestore functions
@@ -11,3 +14,26 @@ var firestoreContext context.Context
 
 // Firebase client used by Firestore functions
 var firebaseClient *firestore.Client
+
+/*
+Sets up Firebase client connection with credentials
+*/
+func InitializeFirestore() {
+	// Firebase initialisation
+	firestoreContext = context.Background()
+
+	// Load credentials from json file containing service account
+	serviceAccount := option.WithCredentialsFile("./assignment2-prog2005-service-account.json")
+	// Create a firebase app with context and credentials
+	app, err := firebase.NewApp(firestoreContext, nil, serviceAccount)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Instantiate client and connect to Firestore
+	firebaseClient, err = app.Firestore(firestoreContext)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
