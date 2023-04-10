1c9d64aabdd6a1d93f0b9f46fba1f26f91590218
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-10 18:43:48 +0200

    feat: Initial setup of firebase

---
+++ utils/db/db_connection.go
@@ -0,0 +1,13 @@
+﻿package db
+
+import (
+	"context"
+
+	"cloud.google.com/go/firestore" // Firestore-specific support
+)
+
+// Firebase context used by Firestore functions
+var firestoreContext context.Context
+
+// Firebase client used by Firestore functions
+var firebaseClient *firestore.Client
