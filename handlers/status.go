7e7864aab37a3e23f62f1573e13352ee292a3824
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-10 13:44:37 +0200

    feat: create Status handler with error handling

--- handlers/status.go
+++ handlers/status.go
@@ -2,8 +2,27 @@ package handlers
 
 import (
 	"net/http"
+	"time"
 )
+
+// Create time of service start variable to calculate uptime
+var Start time.Time
 
 func Status(w http.ResponseWriter, r *http.Request) {
 
+	// Send error if request is not GET:
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid method, currently only GET is supported", http.StatusNotImplemented)
+		return
+	}
+
+	// Generate status response
+	statusRes, err := createStatusResponse(Start)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Handle get request
+	respondToGetRequestWithJSON(w, statusRes)
 }
