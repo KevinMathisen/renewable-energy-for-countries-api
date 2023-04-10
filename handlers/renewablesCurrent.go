054cb58acb331843193720d4047bece4962a46d9
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-10 23:46:15 +0200

    feat: create getCountriesToQuery(...) function and basic functionality

--- handlers/renewablesCurrent.go
+++ handlers/renewablesCurrent.go
@@ -32,3 +32,49 @@ func RenewablesCurrent(w http.ResponseWriter, r *http.Request) {
 	// Respond with list of countryoutput struct encoded as json to user
 	respondToGetRequestWithJSON(w, response)
 }
+
+/*
+Get country code or name, and neighbours parameter from request, then returns appropiate list of countries from these
+
+	w	- Responsewriter
+	r	- Request
+
+	return	- Either empty list of no country specified, or one country, or country and it's neighbours
+*/
+func getCountriesToQuery(w http.ResponseWriter, r *http.Request) ([]string, error) {
+	var countries []string
+
+	// Get country code or name from request
+	countryCodeOrName, err := getCountryCodeOrNameFromRequest(r)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get neigbour bool from request if it is specified
+	neigbours, err := getNeigboursParameterFromRequest(r)
+	if err != nil {
+		return nil, err
+	}
+
+	// If user didn't specify any country
+	if countryCodeOrName == "" {
+		return nil, nil
+	}
+
+	// If the user specified the name only
+	if len(countryCodeOrName) != 3 {
+		// TODO: Implement how to get the ISO code if name is given
+
+		// Else if the user specified ISO code, add the code the list of countries
+	} else {
+		countries = append(countries, countryCodeOrName)
+	}
+
+	// If the user specified the neighbour parameter
+	if neigbours {
+		// TODO: Get neighbour ISO code with Restcountries API, and append to countries
+	}
+
+	return countries, nil
+
+}
