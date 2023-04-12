c293681e04ee3fd2e643c6f12e2ca6b3519547d2
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-12 11:57:06 +0200

    feat: create getHistoryRenewablesForCountires(...) funcion functionality

--- handlers/renewablesHistory.go
+++ handlers/renewablesHistory.go
@@ -43,29 +43,54 @@ func RenewablesHistory(w http.ResponseWriter, r *http.Request) {
 }
 
 /*
-getHistoryRenewablesForCountries(countries, startYear, endYear, wantMean)	//History
+Get renewables data for each year or mean from year range specified, from countires specified
+If the user has no preference for type of data returned, the user will get historical data for each year when specifying countires, and get mean data for all countires if no counties are specified.
 
-	if startYear or endYear not specified
-		set them as default values
+	w			- Responsewriter for sending error messages
+	countries	- Either a list of countires we want to get data from, or an empty list if we want all
+	beginYear	- The first year we will get data from. If -1 we get the default beginYear.
+	endYear		- The last year we will get data from. If -1 we get the default endYear (currentyear)
+	sortByvalue	- If output is to be sorted by percentage value decending
+	getMean		- If the user wants to get mean values, even if countires are specified, this should be true. Does not affect output if no countries are specified, as this will always result in mean value being displayed
 
-
-	if countires specified and not/no mean:
-		getRenewablesForCountriesByYears(isoList, startYear, endYear)					Global1
-
-	if countires specified and mean:
-		getMeanRenewablesFromCountries(isoList, startYear, endYear)						History1
-			for countires
-				getMeanRenewablesFromCountry(isoCode, startYear, endYEar)				History1.1
-
-
-	else if no countires specified and want mean
-		getMeanRenewablesFromAllCountries()												History2
-			for all countires
-				getMeanRenewablesFromCountry(startYear, endYEar)						History2.2
-
-	else if no countries specified and no mean:
-		getRenewablesForAllCountiresByYear(startYear, endYear)							Global2
-
-
-
+	return		- List of countryoutput structs which will be sent as json response. The struct will not have the field "year" defined if mean values are returned.
 */
+func getHistoryRenewablesForCountries(w http.ResponseWriter, countries []string, beginYear int, endYear int, sortByValue bool, getMean bool) ([]structs.CountryOutput, error) {
+	var renewablesOutput []structs.CountryOutput
+
+	// If beginYear not specified set to default
+	if beginYear == -1 {
+		// TODO: Find beginyear
+		beginYear = 1965
+	}
+
+	// If endYear not specified, set to default
+	if endYear == -1 {
+		// TODO: Get current year, as this will be default
+		endYear = 2021
+	}
+
+	// If countires specified and we don't want mean data, get renewables data from them in year range given
+	if len(countries) != 0 && !getMean {
+		renewablesOutput, err := getRenewablesForCountriesByYears(w, countries, beginYear, endYear)
+		if err != nil {
+			return renewablesOutput, err
+		}
+
+	} else if len(countries) != 0 && getMean {
+		// If countires specified and we want mean data, get renewabled mean data from them in year range given
+		renewablesOutput, err := getMeanRenewablesForCountriesByYears(w, countries, beginYear, endYear)
+		if err != nil {
+			return renewablesOutput, err
+		}
+
+	} else if len(countries) == 0 {
+		// If no countries specified, get renewables mean data from all in year range given
+		renewablesOutput, err := getMeanRenewablesForAllCountriesByYears(w, beginYear, endYear)
+		if err != nil {
+			return renewablesOutput, err
+		}
+	} // TODO: Implement functionality for getting renewables history data for all countires which are not mean values
+
+	return renewablesOutput, nil
+}
