2bea25127c809c67fb89db9d011c65b8abc38c25
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-12 23:15:23 +0200

    refactor: change Percentage value in CountryOutput struct to float64

--- utils/structs/structs.go
+++ utils/structs/structs.go
@@ -4,10 +4,10 @@ package structs
 * Struct for encoding json response for RENEWABLES_CURRENT and RENEWABLES_HISTORY endpoints.
  */
 type CountryOutput struct {
-	Name       string `json:"name"`
-	IsoCode    string `json:"isoCode"`
-	Year       string `json:"year,omitempty"` //  suppress field if not defined, such as when returning mean percentage value.
-	Percentage string `json:"percentage"`
+	Name       string  `json:"name"`
+	IsoCode    string  `json:"isoCode"`
+	Year       string  `json:"year,omitempty"` //  suppress field if not defined, such as when returning mean percentage value.
+	Percentage float64 `json:"percentage"`
 }
 
 /*
