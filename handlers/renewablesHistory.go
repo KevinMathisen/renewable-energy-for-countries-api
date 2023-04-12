9793a710d32ee5863f688bd4659f53f83ca7c9fd
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-12 11:29:23 +0200

    feat: create complete renewabledHistory(...) handler functionality

--- handlers/renewablesHistory.go
+++ handlers/renewablesHistory.go
@@ -1,15 +1,49 @@
 package handlers
 
 import (
+	"assignment2/utils/structs"
 	"net/http"
 )
 
 func RenewablesHistory(w http.ResponseWriter, r *http.Request) {
+	var response []structs.CountryOutput
 
+	// Send error message if request method is not get
+	if r.Method != http.MethodGet {
+		http.Error(w, "Invalid method, currently only GET is supported", http.StatusNotImplemented)
+		return
+	}
+
+	// If cache hit, send cached response
+	hit := checkCache(w, r)
+	if hit {
+		return
+	}
+
+	// Get the countries we are interested in finding, or empty if everyone
+	countries, err := getCountriesToQuery(w, r)
+	if err != nil {
+		return
+	}
+
+	// Get parameters if user specified any
+	beginYear, endYear, sortByValue, getMean, err := getRenewablesHistoryParameters(w, r)
+	if err != nil {
+		return
+	}
+
+	// Get the historical percentage of renewables for countires specified as a list of countryoutput structs
+	response, err = getHistoryRenewablesForCountries(w, countries)
+	if err != nil {
+		return
+	}
+
+	// Respond with list of countryoutput struct encoded as json to user
+	respondToGetRequestWithJSON(w, response)
 }
 
 /*
-getHistoryPercentageOfRenewablesForCountries(countries, startYear, endYear, wantMean)	//History
+getHistoryRenewablesForCountries(countries, startYear, endYear, wantMean)	//History
 
 	if startYear or endYear not specified
 		set them as default values
