7edeb897cf9c32e682a2018d55c6014c080a000a
raphaesl <raphaesl@stud.ntnu.no>
2023-04-16 21:42:25 +0200

    refactor: moved error struct and related functions into structs util

--- handlers/errorHandler.go
+++ handlers/errorHandler.go
@@ -1,11 +1,12 @@
 package handlers
 
 import (
+	"assignment2/utils/structs"
 	"log"
 	"net/http"
 )
 
-type RootHandler func(http.ResponseWriter, *http.Request) WrappedError
+type RootHandler func(http.ResponseWriter, *http.Request) structs.WrappedError
 
 // Handles all errors in the same place.
 func (fn RootHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -19,34 +20,4 @@ func (fn RootHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Println("\t" + err.Error())               //Logs original error
 	http.Error(w, err.UsrMessage, err.StatusCode) //Returns user message and error code
 
-}
-
-/*
-* Struct for wrapping errors for standardized error handling.
-*
-* OrigErr: Original error message
-* StatusCode: Status code to show user
-* UsrMessage: Error message to show user.
-* DevMessage: Error message to display in logs.
- */
-type WrappedError struct {
-	OrigErr    error
-	StatusCode int
-	UsrMessage string
-	DevMessage string
-}
-
-// Function for creating a new error message.
-func NewError(origErr error, statusCode int, userMsg, devMsg string) error {
-	return WrappedError{
-		OrigErr:    origErr,
-		StatusCode: statusCode,
-		UsrMessage: userMsg,
-		DevMessage: devMsg,
-	}
-}
-
-// Returns original error in string form
-func (err WrappedError) Error() string {
-	return err.OrigErr.Error()
 }
--- utils/structs/structs.go
+++ utils/structs/structs.go
@@ -19,7 +19,7 @@ type Country struct {
 }
 
 /*
-* Struct for decoding JSON POST request for registration of webhooks in Notification endpoint.
+* Struct for decoding JSON POST request for Registration of Webhooks in Notification endpoint.
  */
 type NewWebhook struct {
 	Url     string `json:"url"`
@@ -61,19 +61,4 @@ type WrappedError struct {
 	StatusCode int
 	UsrMessage string
 	DevMessage string
-}
-
-// Function for creating a new error message.
-func NewError(origErr error, statusCode int, userMsg, devMsg string) error {
-	return WrappedError{
-		OrigErr:    origErr,
-		StatusCode: statusCode,
-		UsrMessage: userMsg,
-		DevMessage: devMsg,
-	}
-}
-
-// Returns original error in string form
-func (err WrappedError) Error() string {
-	return err.OrigErr.Error()
 }
