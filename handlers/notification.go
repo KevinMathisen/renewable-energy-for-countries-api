4609007147bb67007e3658c98c8a81f882a52091
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-19 21:55:30 +0200

    feat: send request to functions based on http method

--- handlers/notification.go
+++ handlers/notification.go
@@ -5,5 +5,22 @@ import (
 )
 
 func Notification(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	var err error
+
+	// Send request to different functions based on method
+	switch r.Method {
+	case http.MethodPost:
+		err = registrationOfWebhook(w, r)
+	case http.MethodDelete:
+		err = deletionOfWebhook(w, r)
+	case http.MethodGet:
+		err = viewWebhook(w, r)
+	default:
+		/*
+			Status: Not impemented
+			Message: Invalid method, currently only [methods supported] is supported
+		*/
+	}
+
+	return err
 }
