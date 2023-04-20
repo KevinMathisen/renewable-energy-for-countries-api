146deb560e4fc31896022e10ea1ceba1b09422a9
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-20 19:55:07 +0200

    fix: update notification endpoint path

--- utils/constants/constants.go
+++ utils/constants/constants.go
@@ -9,7 +9,7 @@ const SERVICE_PATH = "/energy/" + VERSION
 const RENEWABLES_PATH = SERVICE_PATH + "/renewables"
 const RENEWABLES_CURRENT_PATH = RENEWABLES_PATH + "/current/"
 const RENEWABLES_HISTORY_PATH = RENEWABLES_PATH + "/history/"
-const NOTIFICATION_PATH = SERVICE_PATH + "/notification"
+const NOTIFICATION_PATH = SERVICE_PATH + "/notification/"
 const STATUS_PATH = SERVICE_PATH + "/status"
 
 // Content type
