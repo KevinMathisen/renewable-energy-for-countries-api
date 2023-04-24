82049d5e30dc13a8763a7ff879a9df26b2bcab0c
Kevin Nikolai Mathisen <kevinnm@stud.ntnu.no>
2023-04-24 16:23:16 +0200

    refactor: change retrival of neighbours to use less loops

--- utils/params/params.go
+++ utils/params/params.go
@@ -60,21 +60,16 @@ func GetCountriesToQuery(w http.ResponseWriter, r *http.Request, path string) ([
 
 	// If the user specified the neighbour parameter, get neighbour ISO code with Restcountries API
 	if neighbours {
-		//Iterate through each country in the list
-
-		for _, iso := range countries {
-			country, err := gateway.GetCountryByIso(iso) //Get the country object
-			if err != nil {
-				return nil, err
-			}
-			borders := country.Borders       //Retrieve borders from country
-			for _, border := range borders { //Add borders to countries list, this will most likely include a lot of duplicates.
-				countries = append(countries, border)
-			}
-
-			temp := countries                      //Take backup of countries
-			countries = div.RemoveDuplicates(temp) //Remove duplicates from countries
+		country, err := gateway.GetCountryByIso(countries[0]) //Get the country object
+		if err != nil {
+			return nil, err
 		}
+
+		//Add borders to countries list, this will most likely include a lot of duplicates.
+		countries = append(countries, country.Borders...)
+
+		temp := countries                      //Take backup of countries
+		countries = div.RemoveDuplicates(temp) //Remove duplicates from countries
 	}
 
 	// Check if each country exists in the database
@@ -214,8 +209,6 @@ func GetWebhookFromRequest(w http.ResponseWriter, r *http.Request) (structs.Webh
 		log.Println(err.Error())
 		return webhook, structs.NewError(err, 500, constants.DEFAULT500, "There was an error when decoding webhook from json.")
 	}
-
-	log.Println(webhook)
 
 	return webhook, nil
 }
