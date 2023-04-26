c7150a37f172800353427a8089d937a309f22586
Sondre Espeland <sondesp@stud.ntnu.no>
2023-04-26 15:39:58 +0200

    test: implemented HttpRequestFromUrl tests

--- utils/gateway/requests_test.go
+++ utils/gateway/requests_test.go
@@ -52,3 +52,58 @@ func TestRespondToGetRequestWithJSON(t *testing.T) {
 	assert.Contains(t, string(body), expectedBody, "Response body should contain expected body.")
 	assert.Equal(t, expectedContentType, res.Header.Get("content-type"), "Response content type should be equal to expected content type.")
 }
+
+/*
+Tests the HttpRequestFromUrl function
+*/
+func TestHttpRequestFromUrl(t *testing.T) {
+	// Create a test server
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Define body to be marshalled into JSON
+		body := struct {
+			Title string `json:"title"`
+			Msg   string `json:"msg"`
+		}{
+			Title: "Hello",
+			Msg:   "Hello World!",
+		}
+
+		RespondToGetRequestWithJSON(w, body, 200)
+	}))
+
+	defer ts.Close()
+
+	// Make a request to the test server
+	res, err := HttpRequestFromUrl(ts.URL, "GET")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Define expected response body
+	expectedBody := `{"title":"Hello","msg":"Hello World!"}`
+	// Define expected response status code
+	expectedStatus := 200
+	// Expected content type
+	expectedContentType := "application/json"
+
+	assert.Equal(t, expectedStatus, res.StatusCode, "Response status code should be 200.")
+	assert.Contains(t, string(body), expectedBody, "Response body should contain expected body.")
+	assert.Equal(t, expectedContentType, res.Header.Get("content-type"), "Response content type should be equal to expected content type.")
+}
+
+/*
+Tests the HttpRequestFromUrl function with a non-existing URL
+*/
+func TestHttpRequestFromUrlNonExistingUrl(t *testing.T) {
+	// Make a request to the test server
+	_, err := HttpRequestFromUrl("http://localhost:1234", "GET")
+	if err == nil {
+		t.Fatal("Expected error, but got nil.")
+	}
+}
